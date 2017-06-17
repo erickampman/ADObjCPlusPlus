7941f9c65644a1d083e4dbc44eb32fe3edfa6993
Eric Kampman <erickampman@yahoo.com>
2017-06-17 09:12:00 -0700

    Initial Commit

---
+++ ADObjCPlusPlus/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ADObjCPlusPlus
+//
+//  Created by Eric Kampman on 6/17/17.
+//  Copyright © 2017 Eric Kampman. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
---
+++ ADObjCPlusPlus/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  ADObjCPlusPlus
+//
+//  Created by Eric Kampman on 6/17/17.
+//  Copyright © 2017 Eric Kampman. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+	return NSApplicationMain(argc, argv);
+}
